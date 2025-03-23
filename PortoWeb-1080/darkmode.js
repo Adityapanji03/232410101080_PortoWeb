ba13ec6d6622b39c44de2dc356c2073593318542
Adityapanji03 <149854179+Adityapanji03@users.noreply.github.com>
2025-03-22 20:33:52 -0700

    Delete PortoWeb-1080 directory

--- PortoWeb-1080/darkmode.js
+++
@@ -1,8 +0,0 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-const hero = document.body;
-
-darkModeToggle.addEventListener('click', () => {
-    console.log("anjayt")
-    darkModeToggle.classList.toggle('dark');
-    hero.classList.toggle('dark-mode');
-});
